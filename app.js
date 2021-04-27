d511e907855a47d062c898a3c243127b805a8764
Dmitry Nosov <mizai@yandex.ru>
2021-04-27 10:04:36 +0300

    Bugs: POST request failed, json body parser

--- app.js
+++ app.js
@@ -11,29 +11,31 @@ const {
 const app = express();
 
 app.use(helmet());
-
 app.disable('x-powered-by');
-
+app.use(express.json());
 app.use((req, res, next) => {
   req.user = {
     _id: '6084fd8c7ef7452941ce7275',
   };
   next();
 });
-
 app.use(routes);
 
 async function main() {
-  await mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
-  console.log('DB connected');
-  await app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`);
-  });
+  try {
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log('DB connected');
+    await app.listen(PORT, () => {
+      console.log(`App is listening on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(`Connection error, ${err}`);
+  }
 }
 
 main();
