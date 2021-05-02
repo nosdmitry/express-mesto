ceef925c80976e0d1b5a6ae50e1aeb225b98f2d7
Dmitry Nosov <mizai@yandex.ru>
2021-05-03 00:29:46 +0300

    Feature: hashed password sends to DB

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,9 @@
+const validator = require('validator');
+const bcrypt = require('bcrypt');
 const { User } = require('../models/user');
+
+const UNIQUE_EMAIL_ERROR = 11000;
+const SOLT_ROUNDS = 10;
 
 module.exports.getUsers = async (req, res) => {
   try {
@@ -25,17 +30,45 @@ module.exports.getUsersById = async (req, res) => {
   }
 };
 
-module.exports.createUser = async (req, res) => {
-  try {
-    const user = new User(req.body);
-    res.status(200).send(await user.save());
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
-    } else {
-      res.status(500).send({ message: 'Серверная ошибка.' });
-    }
+// module.exports.createUser = async (req, res) => {
+//   try {
+//     const { email } = req.body;
+//     if (validator.isEmail(email)) {
+//       req.body.password = await bcrypt.hash(req.body.password, SOLT_ROUNDS);
+//       const user = new User(req.body);
+//       res.status(200).send(user.save());
+//     } else {
+//       res.status(400).send({ message: 'Некорректный email' });
+//     }
+//   } catch (err) {
+//     if (err.code === UNIQUE_EMAIL_ERROR) {
+//       res.status(409).send({ message: 'Пользователь с указаной почтой уже зарегистрирован'});
+//     } else if (err.name === 'ValidationError') {
+//       res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+//     } else {
+//       res.status(500).send({ message: 'Серверная ошибка.' });
+//     }
+//   }
+// };
+
+module.exports.createUser = (req, res) => {
+  const { email, password } = req.body;
+  if (!validator.isEmail(email)) {
+    return res.status(400).send({ message: 'Некорректный email' });
   }
+  bcrypt.hash(password, SOLT_ROUNDS)
+    .then((hash) => User.create({ ...req.body, password: hash }))
+    .then((createUser) => {
+      res.status(200).send(createUser);
+    })
+    .catch((err) => {
+      if (err.code === UNIQUE_EMAIL_ERROR) {
+        res.status(409).send({ message: 'Пользователь с указаной почтой уже зарегистрирован' });
+      } else {
+        res.status(500).send({ message: 'Серверная ошибка' });
+      }
+      res.status(400).send(err);
+    });
 };
 
 module.exports.updateUserProfile = async (req, res) => {
--- models/user.js
+++ models/user.js
@@ -5,15 +5,24 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 2,
     maxlength: 30,
-    requred: true,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: 'Исследователь',
   },
   avatar: {
+    type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
     type: String,
     required: true,
   },
